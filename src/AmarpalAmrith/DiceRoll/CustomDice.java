579496ca20d6bc28e9cca9c8774abae9f4e0b8ae
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-03-07 13:50:44 +0000

    Dice created Custom dice Class

--- src/AmarpalAmrith/DiceRoll/CustomDice.java
+++ src/AmarpalAmrith/DiceRoll/CustomDice.java
@@ -1,4 +1,19 @@
 package AmarpalAmrith.DiceRoll;
 
-public class CustomDice {
+import java.util.List;
+import java.util.Random;
+
+public class CustomDice implements Dice {
+
+    private List <Integer> sides;
+
+    public CustomDice(List<Integer> sides) {
+        this.sides = sides;
+    }
+
+    @Override
+    public int roll() {
+        Random randomNumberGenerator = new Random();
+        return sides.get(randomNumberGenerator.nextInt(sides.size()));
+    }
 }
