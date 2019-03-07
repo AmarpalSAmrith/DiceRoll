9409c56a777b4d8b7c3759b63eafdc08a43a77bd
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-03-07 14:51:00 +0000

    Dice Part 6

--- src/AmarpalAmrith/DiceRoll/Main.java
+++ src/AmarpalAmrith/DiceRoll/Main.java
@@ -1,56 +1,34 @@
 package AmarpalAmrith.DiceRoll;
 
 import java.util.List;
-import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Utilities.welcomeMsg("Welcome to Dice Roll :)");
-        int sizeOfDice = 0;
-        int numberOfRolls = 0;
 
-        switch (Utilities.requestString("Would you like to use a Custom or Standard Dice").toLowerCase()) {
+        boolean isCustom = "custom".equals(Utilities.requestString("Would you like to use a Custom or Standard Dice").toLowerCase());
 
-            case "custom":
-                sizeOfDice = Utilities.requestInt("Please enter how many faces are on your custom dice?");
-                List <Integer> customFaces = Utilities.fillList(sizeOfDice,"Please enter the number on side ");
-                numberOfRolls = Utilities.requestInt("Please enter how many dice you would like to roll");
-                System.out.println(sizeOfDice + " sided custom dice rolled, " + numberOfRolls + " time(s), gives a total of " + calculateSumOfCustomRolls(numberOfRolls,customFaces));
-                break;
+        Dice dice = isCustom ? createCustomDice() : createStandardDice();
+        int numberOfRolls = Utilities.requestInt("Please enter how many dice you would like to roll");
+        int sumOfRolls = calculateTotalRoll(dice,numberOfRolls);
+        System.out.println("Total thrown: " + sumOfRolls);
+    }
 
-            default:
-                sizeOfDice = Utilities.requestInt("Please enter the size of dice you want to roll. e.g. 4, 6, 8, 12, 20");
-                numberOfRolls = Utilities.requestInt("Please enter how many dice you would like to roll");
-                System.out.println(sizeOfDice + " sided dice rolled, " + numberOfRolls + " time(s), gives a total of " + calculateSumOfRolls(sizeOfDice,numberOfRolls));
-                break;
+    private static Dice createCustomDice() {
+        int numberOfSides = Utilities.requestInt("Please provide the number of faces to your dice");
+        List <Integer> sides = Utilities.fillList(numberOfSides,"Please enter the number on side ");
+        return new CustomDice(sides);
+    }
+    private static Dice createStandardDice() {
+        return new StandardDice(Utilities.requestInt("Please provide the number of sides to your dice"));
+    }
+    public static int calculateTotalRoll(Dice dice, int numberOfThrows) {
+        int sumOfThrows = 0;
+        for (int i = 0; i < numberOfThrows; i++) {
+            sumOfThrows += dice.roll();
         }
-
-    }
-
-    private static int diceRoll(int numberOfSides){
-        Random randomNumberGenerator = new Random();
-        return randomNumberGenerator.nextInt(numberOfSides) + 1;
-    }
-
-    private static int calculateSumOfRolls(int sizeOfDice, int numberOfRolls) {
-        int sumOfRolls = 0;
-        for (int i = 1; i <= numberOfRolls; i++) {
-            sumOfRolls += diceRoll(sizeOfDice);
-        }
-        return sumOfRolls;
-    }
-
-    private static int calculateSumOfCustomRolls(int numberOfRolls, List <Integer> customFaces) {
-        int sumOfRolls = 0;
-        for (int i = 1; i <= numberOfRolls; i++) {
-            sumOfRolls += rollCustomDice(customFaces);
-        }
-        return sumOfRolls;
-    }
-
-    private static int rollCustomDice(List <Integer> customFaces) {
-        return customFaces.get(diceRoll(customFaces.size()) - 1);
+        return sumOfThrows;
     }
 }
